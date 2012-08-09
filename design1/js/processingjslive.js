a4a9eed906501e22a051b27237bb15c63a2c67dd
Rune Madsen <rune@runemadsen.com>
2012-08-09 16:38:34 -0400

    add processing live coding

---
+++ design1/js/processingjslive.js
@@ -0,0 +1,17 @@
+$(function() {
+	
+	_.each($(".processingjslive"), function(liveelement) {
+		
+		var textarea = $(liveelement).find("textarea")[0];
+		var canvas = $(liveelement).find("canvas")[0];
+		var processing = new Processing(canvas, $(textarea).val());
+	
+		$(textarea).before("<div class='processing-header'><h3 class='title'>Processing Console</h3><a href='#' class='processing-runit'>Run</a></div>");
+		
+		$(liveelement).find("a.processing-runit").click(function() {
+			var processing = new Processing(canvas, $(textarea).val());
+			return false;
+		});
+		
+	});
+});
